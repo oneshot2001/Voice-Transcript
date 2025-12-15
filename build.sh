b27ac60d7b1b80980a6577e5a9fc4dcb02e4b0c5
Fred Juhlin <fred.juhlin@gmail.com>
2025-12-15 19:28:21 +0100

    0.9.0

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 rm -rf build
 
 # aarch64 build
-#docker build --progress=plain --no-cache --build-arg ARCH=aarch64 --tag acap .
-#container_id=$(docker create acap)
-#docker cp "$container_id":/opt/app ./build
-#docker rm "$container_id"
-#mv build/*.eap .
-#rm -rf build
+docker build --progress=plain --no-cache --build-arg ARCH=aarch64 --tag acap .
+container_id=$(docker create acap)
+docker cp "$container_id":/opt/app ./build
+docker rm "$container_id"
+mv build/*.eap .
+rm -rf build
 
 # armv7hf build (default)
 docker build --progress=plain --no-cache --tag acap .
